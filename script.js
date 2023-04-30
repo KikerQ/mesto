0493828c85e886a20b785c460e3885a2527c0397
KikerQ <mustwallet@yandex.ru>
2023-05-01 01:11:36 +0700

    первый коммит

---
+++ script.js
@@ -0,0 +1,7 @@
+let edditButton = document.querySelector('.profile__eddit-button');
+console.log(edditButton);
+
+let infoTitle = document.querySelector('.profile-info__title');
+
+
+infoTitle.textContent = "Жуковский А.М";
