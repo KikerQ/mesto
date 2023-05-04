986d3a6e50b8fa7699a0ed8c18befe8bdfcb25a5
KikerQ <mustwallet@yandex.ru>
2023-05-04 16:34:38 +0700

    Исправлен popup form, изменил название классов по БЭМ согласно замечаний

--- script/script.js
+++ script/script.js
@@ -1,38 +1,33 @@
 //поиск классов
-const edditButton = document.querySelector('.profile-info__eddit-button'); //кнопка редактирования фио и проф
+const edditButton = document.querySelector('.profile__eddit-button'); //кнопка редактирования фио и проф
 const popup = document.querySelector('.popup');
-const iconClouse = document.querySelector('.icon-clouse'); //элемент крестик
-const textName = document.querySelector('.profile-info__title'); // Вывод фио на странице
-const textProf = document.querySelector('.profile-info__subtitle'); // Вывод профессии на странице
-const nameInput = document.querySelector('.popup__input_name'); // Инпут фио
-const jobInput = document.querySelector('.popup__input_prof'); // Инпут проф
+const iconClouse = document.querySelector('.popup__btn-clouse'); //элемент крестик
+const textName = document.querySelector('.profile__title'); // Вывод фио на странице
+const textProf = document.querySelector('.profile__subtitle'); // Вывод профессии на странице
+const nameInput = document.querySelector('.popup__form_input_name'); // Инпут фио
+const jobInput = document.querySelector('.popup__form_input_prof'); // Инпут проф
+// Находим форму в DOM
+let formElement = document.querySelector('.popup__form');
 
 
 //функция открытия
 function onPopup () {
-    popup.classList.remove('popup_activ');
+    popup.classList.add('popup_active');
     nameInput.value = textName.textContent; //заполняем инпут
     jobInput.value = textProf.textContent;
 }
 
 //функция закрытия
 function clousePopup () {
-    popup.classList.add('popup_activ');
+    popup.classList.remove('popup_active');
 }
-
-iconClouse.addEventListener('click', clousePopup);//закрыть
-edditButton.addEventListener('click', onPopup); //открыть
-
-
-// Находим форму в DOM
-let formElement = document.querySelector('.popup');
 
 // Обработчик «отправки» формы, хотя пока
 // она никуда отправляться не будет
 function formSubmitHandler (evt) {
-	evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-												// Так мы можем определить свою логику отправки.
-												// О том, как это делать, расскажем позже.
+    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+    // Так мы можем определить свою логику отправки.
+    // О том, как это делать, расскажем позже.
     textName.textContent = nameInput.value;
     textProf.textContent = jobInput.value;
     clousePopup();
@@ -41,7 +36,9 @@ function formSubmitHandler (evt) {
 // Прикрепляем обработчик к форме:
 // он будет следить за событием “submit” - «отправка»
 formElement.addEventListener('submit', formSubmitHandler);
-
+// Слушатель откр и закр формы
+iconClouse.addEventListener('click', clousePopup);//закрыть
+edditButton.addEventListener('click', onPopup); //открыть
 
 
 
