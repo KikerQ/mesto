831929fdf49ac7592349ecad5e03158c68303ac6
KikerQ <mustwallet@yandex.ru>
2023-05-19 17:48:58 +0700

    sp5

--- script/script.js
+++ script/script.js
@@ -1,44 +1,189 @@
 //поиск классов
-const edditButton = document.querySelector('.profile__eddit-button'); //кнопка редактирования фио и проф
-const popup = document.querySelector('.popup');
-const iconClouse = document.querySelector('.popup__btn-clouse'); //элемент крестик
-const textName = document.querySelector('.profile__title'); // Вывод фио на странице
-const textProf = document.querySelector('.profile__subtitle'); // Вывод профессии на странице
+const openPopupEddit = document.querySelector('.popup_open_eddit'); //кнопка редактирования фио и проф
+const openPopupAdd = document.querySelector('.popup_open_add');//кнопка добавления карточек
+const openPopupImage = document.querySelector('.popup_open_image');
+const clouseButtons = document.querySelectorAll('.popup__btn-clouse'); //элемент крестик
+const popupEditProf = document.querySelector('.popup_eddit_profile');
+const popupAddCards = document.querySelector('.popup_add_cards');
+const popupViewImage = document.querySelector('.popup_view_image');
+const textName = document.querySelector('.profile__title'); //вывод фио на странице
+const textProf = document.querySelector('.profile__subtitle'); //вывод профессии на странице
 const nameInput = document.querySelector('.popup__input_value_name'); // Инпут фио
 const jobInput = document.querySelector('.popup__input_value_prof'); // Инпут проф
-// Находим форму в DOM
-let formElement = document.querySelector('.popup__form');
+const placeInput = document.querySelector('.popup__input_value_place'); // Инпут фио
+const linkInput = document.querySelector('.popup__input_value_link'); // Инпут проф
+const formElementCard = document.querySelector('.popup_card_form');
+const formElement = document.querySelector('.popup__form');
+const listCard = document.querySelector('.elements__list');
+const templateItem = document.querySelector('#element_template').content;
+const popupImage = document.querySelector('.popup__image');
+const popupFigcaption = document.querySelector('.popup__figcaption');
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+ 
+//открывания попапов
+const togglePopupState = (popupToToggle) => popupToToggle.classList.toggle('popup_active');
 
+//закрытие попап по крестику
+clouseButtons.forEach(function(e) {
+    e.addEventListener('click', () => {
+        const item = e.closest('.popup');
+        item.classList.remove('popup_active');
+    });
+});
 
-//функция открытия
-function onPopup () {
-    popup.classList.add('popup_active');
+//функция добавления инф в инпуты
+function addInputInfo() {
     nameInput.value = textName.textContent; //заполняем инпут
-    jobInput.value = textProf.textContent;
+    jobInput.value = textProf.textContent; //заполняем инпут
 }
 
-//функция закрытия
-function clousePopup () {
-    popup.classList.remove('popup_active');
-}
-
-// Обработчик «отправки» формы, хотя пока
-// она никуда отправляться не будет
-function formSubmitHandler (evt) {
-    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-    // Так мы можем определить свою логику отправки.
-    // О том, как это делать, расскажем позже.
+// Обработчик «отправки» формы профиля
+function formSubmitHandlerProf(evt) {
+    evt.preventDefault();
     textName.textContent = nameInput.value;
     textProf.textContent = jobInput.value;
-    clousePopup();
+
+    if (textName.textContent !== '' && textProf.textContent !== '') {
+      togglePopupState(popupEditProf);
+    } else {
+      alert('Пожалуйста, заполните все поля ввода');
+      return;   
+    }
+}
+//открыть для ред профиля
+openPopupEddit.addEventListener('click', () => { 
+    togglePopupState(popupEditProf);
+     addInputInfo();
+});
+
+
+//Добавление карточек из массива
+initialCards.forEach( card => {
+  const element = templateItem.cloneNode(true);
+  const cardName = element.querySelector('.element__title');
+  const cardLink = element.querySelector('.element__image');
+  let delButton = element.querySelectorAll('.element__trash');
+  const likeButton = element.querySelector('.element__button');
+  const openPopupImage = element.querySelector('.popup_open_image');
+  cardName.textContent = card.name;
+  cardLink.src = card.link;
+  cardLink.alt = card.name;
+
+    likeButton.addEventListener('click', () => likeButton.classList.toggle('button_like_active'));//ставим лайки 
+  
+  //Открываем попап с картинкой
+  openPopupImage.addEventListener('click', () => {
+    togglePopupState(popupViewImage);
+    popupImage.src = cardLink.src;
+    popupFigcaption.innerText = card.name;
+  });
+
+  listCard.append(element);
+
+  //Удаление карточек
+  delButton.forEach( e => {
+    e.addEventListener('click', () => {
+      const item = e.closest('.element');
+      item.remove();
+    });
+  });
+});
+ 
+//Добавление новой карточки пользователем
+function formSubmitHandlerCard(evt) {
+  evt.preventDefault();
+  //Инфа с инпута
+  const elemName = placeInput.value;
+  const elemLink = linkInput.value;
+  
+  //Проверка полей ввода
+  if (elemName !== '' && elemLink !== '') {
+  
+    //Создаем новую карточку
+    const newCard = templateItem.cloneNode(true);
+    const cardName = newCard.querySelector('.element__title');
+    const cardLink = newCard.querySelector('.element__image');
+    const likeButton = newCard.querySelector('.element__button');
+    const openPopupImage = newCard.querySelector('.popup_open_image');
+    cardName.textContent = elemName;
+    cardLink.src = elemLink;
+    cardLink.alt = elemName;
+      
+    //переключатель лайков
+    likeButton.addEventListener('click', () => likeButton.classList.toggle('button_like_active'));
+      
+    //Открываем попап с картинкой
+    openPopupImage.addEventListener('click', () => {
+      togglePopupState(popupViewImage);
+      popupImage.src = cardLink.src;
+      popupFigcaption.innerText = elemName;
+    });
+
+    //добавление карточки и закрытие
+    listCard.prepend(newCard);
+    togglePopupState(popupAddCards);
+
+  } else {
+    alert('Пожалуйста, заполните все поля ввода');
+    return;
+  }
+  //Удаляем добавленные карточки
+  delButton = document.querySelectorAll('.element__trash');
+
+  delButton.forEach( e => {
+    e.addEventListener('click', () => {
+    const item = e.closest('.element');
+    item.remove();
+    });
+  });
 }
 
-// Прикрепляем обработчик к форме:
-// он будет следить за событием “submit” - «отправка»
-formElement.addEventListener('submit', formSubmitHandler);
-// Слушатель откр и закр формы
-iconClouse.addEventListener('click', clousePopup);//закрыть
-edditButton.addEventListener('click', onPopup); //открыть
+//Чистим поля ввода
+function clearInput () {
+  placeInput.value = '';
+  linkInput.value = '';
+}
+
+
+openPopupAdd.addEventListener('click', () => { togglePopupState(popupAddCards), clearInput()}); //открыть попап для добавления карточек
+formElement.addEventListener('submit', formSubmitHandlerProf);//Обработчик формы профиля
+formElementCard.addEventListener('submit', formSubmitHandlerCard);//Обработчик формы карточек
+
+
+
+
+
+
+
+
+
+
+
 
 
 
