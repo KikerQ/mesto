e69d07d716bad66111f00f9b598d687128aa5c08
KikerQ <mustwallet@yandex.ru>
2023-05-22 22:31:49 +0700

    разнообразил картинки через ИИ

--- script/card.js
+++ script/card.js
@@ -1,26 +1,26 @@
 const initialCards = [
-    {
-      name: 'Архыз',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-    },
-    {
-      name: 'Челябинская область',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-    },
-    {
-      name: 'Иваново',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-    },
-    {
-      name: 'Камчатка',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-    },
-    {
-      name: 'Холмогорский район',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-    },
-    {
-      name: 'Байкал',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-    }
-  ];
+  {
+    name: "Античное здание",
+    link: "https://sun9-80.userapi.com/impg/qot43fNZAC9pUYlYAiuwO4Wn2Q-wLWdPaJmLNQ/tmep9NoW8eY.jpg?size=1024x1024&quality=95&sign=58276785a059d40f3776f736971432a5&type=album",
+  },
+  {
+    name: "Джунгли",
+    link: "https://sun9-68.userapi.com/impg/SGHAni6OabX6eergMw7xHdIx-i7YXzAxpgw8_Q/y3DneohcHRY.jpg?size=1024x1024&quality=95&sign=6e5a158da50bfd227b7072f94c182cbd&type=album",
+  },
+  {
+    name: "Ватикан",
+    link: "https://sun9-22.userapi.com/impg/KBvsxUU-dalOuEM7pLYFQGxMH_Ssaqyu5CfGFg/0vgRqMPJA4I.jpg?size=1024x1024&quality=95&sign=eb2d8af6edc899eb34b987d2ec2a6359&type=album",
+  },
+  {
+    name: "Биг Бен",
+    link: "https://sun9-7.userapi.com/impg/YZVEeD_T5Ai2By1XtDqnleJJzkjzYhkqKcl2ug/j_X_Cm10czg.jpg?size=1024x1024&quality=95&sign=682b49bde60df7d3d7ae05d5368f5481&type=album",
+  },
+  {
+    name: "Пирамида",
+    link: "https://sun9-79.userapi.com/impg/dkcXWH6Hqjm58CG4m8_FUTcgJpyyAoSAfo3R8Q/j7Xmv7YlmOg.jpg?size=1024x1024&quality=95&sign=e22fc78778846e8f17c8d58602c84a35&type=album",
+  },
+  {
+    name: "Китай после...",
+    link: "https://sun9-65.userapi.com/impg/VEBiS6nym-p56By_ohrydEYKkZ9UZyj3ysNJPw/F3jzO2zG_pE.jpg?size=1024x1024&quality=95&sign=e8884a5c3b44a4a7a5af24705aa02bce&type=album",
+  },
+];
--- script/script.js
+++ script/script.js
@@ -1,48 +1,55 @@
 //поиск классов
-const edditButtonPopup = document.querySelector('.profile__eddit-button'); //кнопка редактирования фио и проф
-const addButtonPopup = document.querySelector('.profile__add-button');//кнопка добавления карточек
-const openPopupImage = document.querySelector('.element__image');
-const clouseButtons = document.querySelectorAll('.popup__btn-clouse'); //элемент крестик
-const popupEditProf = document.querySelector('.popup_eddit_profile');
-const popupAddCards = document.querySelector('.popup_add_cards');
-const popupViewImage = document.querySelector('.popup_view_image');
-const nameProfile = document.querySelector('.profile__title'); //вывод фио на странице
-const textProf = document.querySelector('.profile__subtitle'); //вывод профессии на странице
-const inputNameFormProfile = document.querySelector('.popup__input_value_name'); // Инпут фио
-const inputProfessionFormProfile = document.querySelector('.popup__input_value_prof'); // Инпут проф
-const inputNamePlaceFormAddNewCard  = document.querySelector('.popup__input_value_place'); // Инпут названия места
-const inputLinkFormAddNewCard  = document.querySelector('.popup__input_value_link'); // Инпут ссылка на картинку
-const formElementCard = document.querySelector('.popup__form-card');
-const formElementProfile = document.querySelector('.popup__form');
-const listCard = document.querySelector('.elements__list');
-const templateItem = document.querySelector('template');
-const photoPopupViewImage = document.querySelector('.popup__image');
-const subtitlePopupFigcaption = document.querySelector('.popup__figcaption');
- 
+const edditButtonPopup = document.querySelector(".profile__eddit-button"); //кнопка редактирования фио и проф
+const addButtonPopup = document.querySelector(".profile__add-button"); //кнопка добавления карточек
+const openPopupImage = document.querySelector(".element__image");
+const clouseButtons = document.querySelectorAll(".popup__btn-clouse"); //элемент крестик
+const popupEditProf = document.querySelector(".popup_eddit_profile");
+const popupAddCards = document.querySelector(".popup_add_cards");
+const popupViewImage = document.querySelector(".popup_view_image");
+const nameProfile = document.querySelector(".profile__title"); //вывод фио на странице
+const textProf = document.querySelector(".profile__subtitle"); //вывод профессии на странице
+const inputNameFormProfile = document.querySelector(".popup__input_value_name"); // Инпут фио
+const inputProfessionFormProfile = document.querySelector(
+  ".popup__input_value_prof"
+); // Инпут проф
+const inputNamePlaceFormAddNewCard = document.querySelector(
+  ".popup__input_value_place"
+); // Инпут названия места
+const inputLinkFormAddNewCard = document.querySelector(
+  ".popup__input_value_link"
+); // Инпут ссылка на картинку
+const formElementCard = document.querySelector(".popup__form-card");
+const formElementProfile = document.querySelector(".popup__form");
+const listCard = document.querySelector(".elements__list");
+const templateItem = document.querySelector("template");
+const photoPopupViewImage = document.querySelector(".popup__image");
+const subtitlePopupFigcaption = document.querySelector(".popup__figcaption");
+
 //открывания попапов
-const openPopup = (popupToOpen) => popupToOpen.classList.add('popup_active');
+const openPopup = (popupToOpen) => popupToOpen.classList.add("popup_active");
 //закрывание попапов
-const clousePopup = (popupToClouse) => popupToClouse.classList.remove('popup_active');
+const clousePopup = (popupToClouse) =>
+  popupToClouse.classList.remove("popup_active");
 
 //сброс дефолтов
 function resetDefault(evt) {
-  evt.preventDefault()
-};
+  evt.preventDefault();
+}
 
 //функция добавления инф в инпуты
 function addInfoIntputProfile() {
-    inputNameFormProfile.value = nameProfile.textContent; 
-    inputProfessionFormProfile.value = textProf.textContent; 
-};
+  inputNameFormProfile.value = nameProfile.textContent;
+  inputProfessionFormProfile.value = textProf.textContent;
+}
 
 // Обработчик «отправки» формы профиля
 function submitFormHandlerProf(evt) {
-  resetDefault(evt)
+  resetDefault(evt);
   nameProfile.textContent = inputNameFormProfile.value;
   textProf.textContent = inputProfessionFormProfile.value;
   clousePopup(popupEditProf);
-};
-   
+}
+
 //Удаление карточек
 function delCardElementTempalte(itemToDel) {
   itemToDel.remove();
@@ -50,76 +57,82 @@ function delCardElementTempalte(itemToDel) {
 
 //переключатель лайков
 function toggleLike(nameBtn) {
-  nameBtn.classList.toggle('button_like_active')
-};
+  nameBtn.classList.toggle("button_like_active");
+}
 
 //создание карточки
 function createCard(card) {
-  const elementTemplate = templateItem.content.querySelector('.element').cloneNode(true);
-  const cardName = elementTemplate.querySelector('.element__title')
-  const cardLink = elementTemplate.querySelector('.element__image');
-  const delButton = elementTemplate.querySelector('.element__trash');
-  const likeButton = elementTemplate.querySelector('.element__button');
-  const openPopupImage = elementTemplate.querySelector('.element__image');
+  const elementTemplate = templateItem.content
+    .querySelector(".element")
+    .cloneNode(true);
+  const cardName = elementTemplate.querySelector(".element__title");
+  const cardLink = elementTemplate.querySelector(".element__image");
+  const delButton = elementTemplate.querySelector(".element__trash");
+  const likeButton = elementTemplate.querySelector(".element__button");
+  const openPopupImage = elementTemplate.querySelector(".element__image");
   cardName.textContent = card.name;
   cardLink.src = card.link;
   cardLink.alt = card.name;
-  
+
   //ставим лайки
-  likeButton.addEventListener('click', () => toggleLike(likeButton)) 
+  likeButton.addEventListener("click", () => toggleLike(likeButton));
 
-   //Удаление карточек
-   delButton.addEventListener('click', () => delCardElementTempalte(elementTemplate));
+  //Удаление карточек
+  delButton.addEventListener("click", () =>
+    delCardElementTempalte(elementTemplate)
+  );
 
-   //Открываем попап с картинкой
-  openPopupImage.addEventListener('click', () => {
+  //Открываем попап с картинкой
+  openPopupImage.addEventListener("click", () => {
     openPopup(popupViewImage);
     photoPopupViewImage.src = cardLink.src;
     subtitlePopupFigcaption.innerText = card.name;
   });
 
-  return elementTemplate
+  return elementTemplate;
 }
 
 //вставка карточки на страницу (универсальная функция)
 function renderCard(card, conteiner) {
   conteiner.prepend(createCard(card));
-}; 
+}
 
 //Добавление карточек из массива
-initialCards.forEach(item => {renderCard(item, listCard)});
+initialCards.forEach((item) => {
+  renderCard(item, listCard);
+});
 
 //Добавление новой карточки пользователем
 function handlerAddCard(evt) {
-  resetDefault(evt)
+  resetDefault(evt);
   //собираем объект data
   const data = {
-    name: inputNamePlaceFormAddNewCard .value,
-    link: inputLinkFormAddNewCard .value
-  }
-renderCard(data, listCard);
-clousePopup(popupAddCards); 
-};
+    name: inputNamePlaceFormAddNewCard.value,
+    link: inputLinkFormAddNewCard.value,
+  };
+  renderCard(data, listCard);
+  clousePopup(popupAddCards);
+}
 
 //закрытие попап по крестику
-clouseButtons.forEach(function(e) {
-  e.addEventListener('click', () => {
-      const item = e.closest('.popup');
-      clousePopup(item);
+clouseButtons.forEach(function (e) {
+  e.addEventListener("click", () => {
+    const item = e.closest(".popup");
+    clousePopup(item);
   });
 });
 
 //открыть попап для добавления карточек
-addButtonPopup.addEventListener('click', () => {
-   openPopup(popupAddCards);
-   formElementCard.reset();//очистка форм
-}); 
+addButtonPopup.addEventListener("click", () => {
+  openPopup(popupAddCards);
+  formElementCard.reset(); //очистка форм
+});
 
-formElementProfile.addEventListener('submit', submitFormHandlerProf);//Обработчик формы профиля
-formElementCard.addEventListener('submit', handlerAddCard);//Обработчик формы карточек
+formElementProfile.addEventListener("submit", submitFormHandlerProf); //Обработчик формы профиля
+formElementCard.addEventListener("submit", handlerAddCard); //Обработчик формы карточек
 
 //открыть для ред профиля
-edditButtonPopup.addEventListener('click', () => { 
+edditButtonPopup.addEventListener("click", () => {
   openPopup(popupEditProf);
-   addInfoIntputProfile();
+  addInfoIntputProfile();
 });
