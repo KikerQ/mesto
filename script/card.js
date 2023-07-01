212b6d3da1fd5235b089fbbc4ad7499ba34e372d
KikerQ <mustwallet@yandex.ru>
2023-07-01 22:45:32 +0700

    ООП СП7

--- script/card.js
+++ script/card.js
@@ -10,14 +10,14 @@ export default class Card {
       .content.querySelector(".element")
       .cloneNode(true);
   }
-  _setEventListener(cardElement, likeButton, deleteButton, cardImage) {
+  _setEventListener(cardElement, likeButton, deleteButton) {
     likeButton.addEventListener("click", () => {
       likeButton.classList.toggle("button_like_active");
     });
     deleteButton.addEventListener("click", () => {
       cardElement.remove();
     });
-    cardImage.addEventListener("click", () => {
+    this.cardImage.addEventListener("click", () => {
       this._handleOpenPopup(this._data.name, this._data.link);
     });
   }
@@ -25,16 +25,12 @@ export default class Card {
     const cardEl = this._getTemplate();
     const likeButton = cardEl.querySelector(".element__button");
     const deleteButton = cardEl.querySelector(".element__trash");
-    const cardImage = cardEl.querySelector(".element__image");
-    
-    this._setEventListener(cardEl, likeButton, deleteButton, cardImage);
-    
-    cardImage.src = this._data.link;
-    cardImage.alt = this._data.name;
-    
+    this.cardImage = cardEl.querySelector(".element__image");
+    this.cardImage.src = this._data.link;
+    this.cardImage.alt = this._data.name;
     const cardTitle = cardEl.querySelector(".element__title");
     cardTitle.textContent = this._data.name;
-    
+    this._setEventListener(cardEl, likeButton, deleteButton);
     return cardEl;
   }
 }
